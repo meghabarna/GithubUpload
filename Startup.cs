e8193731bc6ce37b5eb1b9d67c459690d971b6d2
meghabarna <59246455+meghabarna@users.noreply.github.com>
2019-12-26 14:30:53 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHubTestProject.Startup))]
+namespace GitHubTestProject
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
